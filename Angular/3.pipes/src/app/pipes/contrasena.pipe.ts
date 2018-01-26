1e08eda483e60e95adf552dc3cb08262a821ed6e
thavoo <gustavoh.2312@gmail.com>
2018-01-26 04:58:36 -0400

    add other forms password pipe

--- Angular/3.pipes/src/app/pipes/contrasena.pipe.ts
+++ Angular/3.pipes/src/app/pipes/contrasena.pipe.ts
@@ -7,16 +7,34 @@ export class ContrasenaPipe implements PipeTransform {
 
   transform(value: string, activar: boolean = true): string {
 
-  if(activar){
-    let salida:string = "";
-    for(let i=0; i< value.length; i++){
-      salida += "*";
-    }
-      return salida;
-  }else{
-    return value;
-  }
+          if(activar){
+            let salida:string = "";
+            for(let i=0; i< value.length; i++){
+              salida += "*";
+            }
+              return salida;
+          }else{
+            return value;
+          }
 
+          /* otra forma de solucionar
+
+            if(activar) {
+            return value.replace(/./g, '*');
+          }else{
+          return value;
+        }
+          */
+
+          /* otra forma
+            return activar ? value.replace(/./g, '*') : value;
+          */
+
+          /*
+          if(!mostrar)
+              return "*".repeat(value.length);
+            return value;
+          */
 
   }
 
