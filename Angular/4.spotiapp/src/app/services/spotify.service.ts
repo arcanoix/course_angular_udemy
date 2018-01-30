95ad6d3a0f796ed77b16218f03147d3a2f7b1ac9
thavoo <gustavoh.2312@gmail.com>
2018-01-29 23:39:28 -0400

    import httpclient and create method getArtistas

--- Angular/4.spotiapp/src/app/services/spotify.service.ts
+++ Angular/4.spotiapp/src/app/services/spotify.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient} from "@angular/common/http";
+
 @Injectable()
 export class SpotifyService {
 
-  constructor() { }
+  constructor( public http:HttpClient) {
+      console.log("servicio de spotify listo");
+  }
+
+  getArtistas(){
+    let url = "https://api.spotify.com/v1/search?query=Muse&type=artist&market=US&limit=20";
+
+      this.http.get(url)
+      .subscribe(respuesta => {
+          console.log(respuesta);
+      });
+
+  }
 
 }
