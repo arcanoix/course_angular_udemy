2462692ce532417e6c05e49aa8a0a82f6ad74b5f
thavoo <gustavoh.2312@gmail.com>
2018-01-30 00:19:20 -0400

    create var termino, add condition for prevent request invalid

--- Angular/4.spotiapp/src/app/components/search/search.component.ts
+++ Angular/4.spotiapp/src/app/components/search/search.component.ts
@@ -9,14 +9,24 @@ import { SpotifyService } from '../../services/spotify.service';
 })
 export class SearchComponent {
 
+  termino: string = '';
+
   constructor(public _spotify: SpotifyService) {
 
-      this._spotify.getArtistas() // <-- se encuentra dentro del _spotify (servicio)
-        .subscribe( artistas => { // subscribe es utilizado solo para ejecutar logica especializada solo en dicho componente
-          console.log("informacion lista");
-          console.log(artistas);
-        });
-        
+
+
+  }
+
+  buscarArtista(){
+    if(this.termino.length == 0 ){
+      return; //esto es para prevenir enviar peticiones invalidad o enviar arreglos vacios
+    }
+
+    this._spotify.getArtistas( this.termino ) // <-- se encuentra dentro del _spotify (servicio)
+      .subscribe( artistas => { // subscribe es utilizado solo para ejecutar logica especializada solo en dicho componente
+        console.log("informacion lista");
+        console.log(artistas);
+      });
   }
 
 
