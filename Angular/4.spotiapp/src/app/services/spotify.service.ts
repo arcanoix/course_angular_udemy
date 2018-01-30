94762d834d44012a386bce17040205d419b2ef3d
thavoo <gustavoh.2312@gmail.com>
2018-01-29 23:52:25 -0400

    update services

--- Angular/4.spotiapp/src/app/services/spotify.service.ts
+++ Angular/4.spotiapp/src/app/services/spotify.service.ts
@@ -1,21 +1,24 @@
 import { Injectable } from '@angular/core';
 
-import { HttpClient} from "@angular/common/http";
+import { HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Injectable()
 export class SpotifyService {
+
+  artistas: any[] = [];
 
   constructor( public http:HttpClient) {
       console.log("servicio de spotify listo");
   }
 
   getArtistas(){
-    let url = "https://api.spotify.com/v1/search?query=Muse&type=artist&market=US&limit=20";
+    let url = "https://api.spotify.com/v1/search?query=Metallica&type=artist&market=US&limit=20";
+    let headers = new HttpHeaders({
+        'authorization':'Bearer BQASTOcVYmUyVUn2W0nsMMcvHM-78f7OIGAxTrMBpYBPDSBknAD0lreA-u3QeC9ZLOKffQI4sYuzuAHf8CejTsUZICFbCxZN8Fu8DWv8q3GUMCMgRfaCAsSCt5CzDmlK-eEy-cB7GwDh0nvgzTMSaccQA4Yi5m04Z-NvfnaUjWRDgYA'
+    });
 
-      this.http.get(url)
-      .subscribe(respuesta => {
-          console.log(respuesta);
-      });
+      return this.http.get(url, { headers }) //esto es un observable
+
 
   }
 
