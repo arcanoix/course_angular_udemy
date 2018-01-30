19994792eace26d263983fe72a081261825e2aab
thavoo <gustavoh.2312@gmail.com>
2018-01-29 23:52:03 -0400

    conect observable with subscribes

--- Angular/4.spotiapp/src/app/components/search/search.component.ts
+++ Angular/4.spotiapp/src/app/components/search/search.component.ts
@@ -10,7 +10,11 @@ import { SpotifyService } from '../../services/spotify.service';
 export class SearchComponent {
 
   constructor(public _spotify: SpotifyService) {
-      this._spotify.getArtistas();
+      this._spotify.getArtistas() // <-- se encuentra dentro del _spotify (servicio)
+        .subscribe( resp => { // subscribe es para procesar la informacion
+          console.log("informacion lista");
+          console.log(resp);
+        })
   }
 
 
