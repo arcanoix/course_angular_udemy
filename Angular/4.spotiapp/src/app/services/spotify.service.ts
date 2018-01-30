4690d84950be9acd0b8be5de88429b60844d9e19
thavoo <gustavoh.2312@gmail.com>
2018-01-30 00:19:58 -0400

    change data url add variable termino for search artists

--- Angular/4.spotiapp/src/app/services/spotify.service.ts
+++ Angular/4.spotiapp/src/app/services/spotify.service.ts
@@ -13,8 +13,8 @@ export class SpotifyService {
       console.log("servicio de spotify listo");
   }
 
-  getArtistas(){
-    let url = "https://api.spotify.com/v1/search?query=Metallica&type=artist&market=US&limit=20";
+  getArtistas(termino: string){
+    let url = `https://api.spotify.com/v1/search?query=${ termino }&type=artist&market=US&limit=20`;
     let headers = new HttpHeaders({
         'authorization':'Bearer BQASTOcVYmUyVUn2W0nsMMcvHM-78f7OIGAxTrMBpYBPDSBknAD0lreA-u3QeC9ZLOKffQI4sYuzuAHf8CejTsUZICFbCxZN8Fu8DWv8q3GUMCMgRfaCAsSCt5CzDmlK-eEy-cB7GwDh0nvgzTMSaccQA4Yi5m04Z-NvfnaUjWRDgYA'
     });
