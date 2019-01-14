2bb10b53db7e0dc1238e0c1cdc74fc1c7d6ee7e4
Gustavo Herrera <gustavoh.2312@gmail.com>
2019-01-14 18:23:54 -0500

    variables, arrow functions, destructuracion de objets y arreglos, promesas

--- TypeScriptES6/app.js
+++ TypeScriptES6/app.js
@@ -1,4 +1,3 @@
-"use strict";
 function saludar(nombre) {
     console.log("Hola " + nombre.toUpperCase());
 }
@@ -16,7 +15,7 @@ var edad = 33;
 //let texto = "Hola, " + nombre + " " + apellido + "(" + edad + ")";
 //esto seria la forma usando platnilla literal del ES6
 var texto = " Hola " + nombre + " " + apellido + " " + (edad);
-//console.log(texto);
+console.log(texto);
 /*
   funciones obligatoria, por defecto y opcional
 */
@@ -31,4 +30,62 @@ function activar(quien, objeto, momento) {
     }
     console.log(mensaje);
 }
-activar("gordon", "batiseñal", "tarde");
+activar("gordon", "batiseñal", "noche");
+//------------------------------------------------------
+// variables
+var nombres;
+var apellidos;
+var mayor;
+var telefono;
+var hoy = new Date();
+var cualquier;
+var spiderman = {
+    nombres: "peter",
+    edad: 20
+};
+// funciones de flecha
+// 
+var mifuncion = function (a) {
+    return a;
+};
+var mifunctionF = function (a) { return a; };
+var mifunction2 = function (a, b) { return a + b; };
+var mifunction3 = function (nombre) {
+    nombre = nombre.toUpperCase();
+    return nombre;
+};
+var hul = {
+    nombre: "hulk",
+    smash: function () {
+        var _this = this;
+        setTimeout(function () { return console.log(_this.nombre + " Smach!!"); }, 1500);
+    }
+};
+hul.smash();
+// destructuracion de objetos y arreglos
+var avenger = {
+    nombrex: "steve",
+    clave: "Capitan America",
+    poder: "droga"
+};
+//let nombre = avenger.nombre;
+//let clave = avenger.clave;
+//let poder = avenger.poder;
+//destructuracion de objetos
+var nombrex = avenger.nombrex, clave = avenger.clave, poder = avenger.poder;
+console.log(nombrex, clave, poder);
+// destructuracion de arreglos
+var avengers = ["thor", "steve", "hulk"];
+var thor = avengers[0], capi = avengers[1], hulk = avengers[2];
+// promesas
+var prom1 = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        console.log('promesa terminada');
+        resolve();
+    }, 1500);
+});
+prom1.then(function () {
+    console.log("ejecutarme cuando este lista");
+}, function () {
+    console.log("ejecutarme si sale todo mal");
+});
--- TypeScriptES6/app.ts
+++ TypeScriptES6/app.ts
@@ -22,7 +22,7 @@
   //esto seria la forma usando platnilla literal del ES6
   let texto = ` Hola ${ nombre } ${ apellido } ${ (edad) }`;
 
-  //console.log(texto);
+  console.log(texto);
 
   /*
     funciones obligatoria, por defecto y opcional
@@ -44,5 +44,76 @@
       console.log(mensaje);
     }
 
-    activar("gordon", "batiseñal" ,"tarde");
+    activar("gordon", "batiseñal" ,"noche");
     //------------------------------------------------------
+
+    // variables
+    let nombres:string;
+    let apellidos:string;
+    let mayor:boolean;
+    let telefono:number;
+    let hoy:Date = new Date();
+    let cualquier:any;
+
+    let spiderman = {
+        nombres:"peter",
+        edad:20
+    }
+
+    // funciones de flecha
+    // 
+    let mifuncion = function(a){
+      return a;
+    }
+
+    let mifunctionF = (a:number) => a;
+
+    let mifunction2 = (a:number, b:number) => a + b;
+
+    let mifunction3 = (nombre:string) => {
+      nombre = nombre.toUpperCase();
+      return nombre;
+    }
+
+    let hul = {
+      nombre: "hulk",
+      smash() {
+        setTimeout( () => console.log(this.nombre + " Smach!!"), 1500);
+      }
+    }
+
+    hul.smash();
+
+    // destructuracion de objetos y arreglos
+    let avenger = {
+      nombrex: "steve",
+      clave: "Capitan America",
+      poder: "droga"
+    }
+
+    //let nombre = avenger.nombre;
+    //let clave = avenger.clave;
+    //let poder = avenger.poder;
+    //destructuracion de objetos
+    let { nombrex, clave, poder } = avenger;
+
+    console.log(nombrex, clave, poder);
+
+    // destructuracion de arreglos
+    let avengers:string[] = ["thor","steve","hulk"];
+    let   [thor, capi, hulk] = avengers;
+
+    // promesas
+    let prom1 = new Promise( function(resolve, reject) {
+      setTimeout( () => {
+          console.log('promesa terminada')
+          resolve()
+       }, 1500)
+
+    } );
+
+    prom1.then(function() {
+        console.log("ejecutarme cuando este lista");
+    }, function() {
+        console.log("ejecutarme si sale todo mal");
+    });
